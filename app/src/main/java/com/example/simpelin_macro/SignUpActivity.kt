596da173915cc462878e0eb7379794e3ba41789d
Muhammad Revanza <rvanza453@gmail.com>
2023-10-17 08:57:50 +0700

    first commit

--- app/src/main/java/com/example/simpelin_macro/SignUpActivity.kt
+++ app/src/main/java/com/example/simpelin_macro/SignUpActivity.kt
@@ -1,11 +1,27 @@
 package com.example.simpelin_macro
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
 
-class SignUpActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        val btnSignup = findViewById<ImageButton>(R.id.btn_signup)
+        btnSignup.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View) {
+        when(v.id){
+            R.id.btn_signup -> {
+                val intent = Intent(this@SignUpActivity,LoginActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
